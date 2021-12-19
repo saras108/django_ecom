091fdbd952996edc89a8c7c73b91ca9f7d8fac89
Saras <saras.me108@gmail.com>
2021-12-19 23:26:53 +0545

    CRUD Operation Started

--- learn/account/views.py
+++ learn/account/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 from .models import *
+
+from .forms import OrderForm
 
 # Create your views here.
 
@@ -34,3 +36,45 @@ def customer(request , pk):
 
     context = {'customer': customer , 'orders': orders , 'orders_count' : orders_count}
     return render(request , 'accounts/customer.html' , context)
+
+
+def createOrder(request):
+    form = OrderForm()
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+
+        if(form.is_valid()):
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request , 'accounts/order_form.html' , context)
+
+
+def updateOrder(request , pk):
+
+    order = Order.objects.get(id = pk)
+    form = OrderForm(instance= order)
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST , instance=order)
+
+        if(form.is_valid()):
+            form.save()
+            return redirect('/')
+    
+    context = {'form': form}
+    return render(request , 'accounts/order_form.html' , context)
+
+def deleteOrder(request , pk):
+    order = Order.objects.get(id = pk)
+
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+    
+    context = {'order': order}
+
+
+    return render(request , 'accounts/delete.html' , context)
