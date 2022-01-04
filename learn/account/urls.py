08c492de8e9e28cc29a66d7a8f2c94d155288d53
Saras <saras.me108@gmail.com>
2022-01-04 20:04:02 +0545

    password can be reset by sending mail to users account

--- learn/account/urls.py
+++ learn/account/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
-
 
 urlpatterns = [
 
@@ -19,4 +19,17 @@ urlpatterns = [
     path('create_order/', views.createOrder , name='create_order'),
     path('update_order/<str:pk>/', views.updateOrder , name='update_order'),
     path('delete_order/<str:pk>/', views.deleteOrder , name='delete_order'),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name = "accounts/password_reset.html"),
+        name='reset_password'),
+
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name = "accounts/password_reset_sent.html"),
+        name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name = "accounts/password_reset_form.html"),
+         name='password_reset_confirm'),
+
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name = "accounts/password_reset_done.html") ,
+        name='password_reset_complete'),
+
 ]
